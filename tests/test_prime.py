50dce2ce3ca95fb95f98ae21fd6a075b5d9f4e29
Jagoda Jeczmien-Lazur <jagoda.jeczmienlazur@gmail.com>
2022-11-06 16:33:37 +0100

    #3

--- tests/test_prime.py
+++ tests/test_prime.py
@@ -16,3 +16,11 @@ def test_param_one():
     factors = []
     value = 1
     assert len(prime.generate_prime_factors(value, factors)) == 0
+
+
+def test_param_two():
+    factors = []
+    value = 2
+    output_list = prime.generate_prime_factors(value, factors)
+
+    assert len(output_list) == 1 and output_list[0] == 2
